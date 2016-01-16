25cd3750e124bb2e940a4871db5b4bc9038e08c8
Feross Aboukhadijeh <feross@feross.org>
2016-01-16 22:30:38 +0100

    remove unneeded comments

--- config.rb
+++ config.rb
@@ -12,13 +12,3 @@ javascripts_dir = "js/compiled"
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 relative_assets = true
-
-# To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
-
-
-# If you prefer the indented syntax, you might want to regenerate this
-# project again passing --syntax sass, or you can uncomment this:
-# preferred_syntax = :sass
-# and then run:
-# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
