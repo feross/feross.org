0a06ad4b5ce842c99c93e4d65d79832fcc6ea4d2
Feross Aboukhadijeh <feross@feross.org>
2016-01-17 17:17:53 +0100

    use browserify

---
+++ client/index.js
@@ -0,0 +1,97 @@
+var $ = require('jquery')
+var addCommas = require('add-commas')
+var throttle = require('throttleit')
+
+$(document).ready(function () {
+  // If this is homepage, get view count for all posts
+  if ($('body').hasClass('home')) {
+    $.ajax({
+      type: 'GET',
+      url: '/views/total',
+      dataType: 'json',
+      success: function (data) {
+        var views = addCommas(data.views)
+        $('.views').text(views)
+      },
+      error: function (data) {
+        $('.views').text('Lots of')
+      }
+    })
+  }
+
+  // If this is a post, get post view count
+  if ($('body').hasClass('post')) {
+    var slug = $('.views').data('slug')
+    $.ajax({
+      type: 'POST',
+      url: '/views',
+      data: {slug: slug},
+      dataType: 'json',
+      success: function (data) {
+        var views = addCommas(data.views)
+        $('.views').text(views + ' views')
+      },
+      error: function (data) {
+        $('.views').text('Lots of views')
+      }
+    })
+  }
+})
+
+// Fade prev/next post links on scroll
+if ($('body').hasClass('post')) {
+  var maxOpacity = 0.7
+  var minOpacity = 0.2
+
+  var $header = $('#header')
+  var headerBottom = $header.offset().top + $header.height()
+
+  var $meta = $('.meta')
+  var $copy = $('.copy')
+  var copyOffset = 100 // start fading in a little early
+  var copyTop = $meta.offset().top - copyOffset // using meta, since that includes h3
+  var copyBottom = $copy.offset().top + $copy.height() - copyOffset
+  var copyHeight = copyBottom - copyTop
+
+  var $window = $(window)
+  var $navLinks = $('.prev, .next')
+  var $nextLink = $('.next')
+  var $ad = $('.carbonad')
+
+  var onScrollOrResize = function (event) {
+    var windowHeight = $window.height()
+    var windowWidth = $window.width()
+    var windowScrollTop = $window.scrollTop()
+    var windowBottom = windowScrollTop + windowHeight
+
+    $nextLink.removeClass('hidden')
+
+    var opacity
+    if (windowScrollTop < 0) {
+      opacity = maxOpacity
+    } else if (windowScrollTop <= headerBottom) {
+      opacity = maxOpacity - ((windowScrollTop / headerBottom) * (maxOpacity - minOpacity))
+    } else if (windowBottom < copyTop) {
+      opacity = minOpacity
+    } else if (windowBottom < copyBottom) {
+      opacity = minOpacity + ((windowBottom - copyTop) / copyHeight * (maxOpacity - minOpacity))
+    } else {
+      opacity = maxOpacity
+    }
+
+    $navLinks.css({opacity: opacity})
+
+    // if next link is overlapping with ad, hide it
+    if ($nextLink.length && windowWidth > 950 && windowWidth < 1225) {
+      var nextLinkTop = $nextLink.offset().top
+      var adBottom = $ad.offset().top + $ad.height() - 50 // no idea why it's 50px off...
+      if (nextLinkTop < adBottom) {
+        $nextLink.addClass('hidden')
+      }
+    }
+  }
+
+  window.addEventListener('scroll', throttle(onScrollOrResize, 150))
+  window.addEventListener('resize', throttle(onScrollOrResize, 150))
+  onScrollOrResize()
+}
