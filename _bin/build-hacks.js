c0aa225f2c645d7ac72cf6826be5fdd8e0c785e3
Feross Aboukhadijeh <feross@feross.org>
2018-12-19 17:48:33 -0800

    alphabetize build-hacks git repos

--- _bin/build-hacks.js
+++ _bin/build-hacks.js
@@ -5,15 +5,15 @@ const path = require('path')
 const series = require('run-series')
 
 const MODULES = [
-  ['git@github.com:feross/detect-proxy.git', 'detect-proxy'],
-  ['git@github.com:feross/Fullscreen-API-Attack.git', 'fullscreen-api-attack'],
-  ['git@github.com:feross/Nim.js.git', 'nim.js'],
-  ['git@github.com:feross/Facebook-Like-Everything.git', 'facebook-like-everything'],
-  ['git@github.com:feross/Selective-Attention-Test.git', 'selective-attention-test'],
-  ['git@github.com:feross/webcam-spy.git', 'webcam-spy'],
   ['git@github.com:feross/ahh-windows.git', 'ahh-windows'],
+  ['git@github.com:feross/detect-proxy.git', 'detect-proxy'],
+  ['git@github.com:feross/Facebook-Like-Everything.git', 'facebook-like-everything'],
+  ['git@github.com:feross/Fullscreen-API-Attack.git', 'fullscreen-api-attack'],
   ['git@github.com:feross/md5-password-cracker.js.git', 'md5-password-cracker.js'],
-  ['git@github.com:feross/SuperTranslate.git', 'instant.io']
+  ['git@github.com:feross/Nim.js.git', 'nim.js'],
+  ['git@github.com:feross/Selective-Attention-Test.git', 'selective-attention-test'],
+  ['git@github.com:feross/SuperTranslate.git', 'instant.io'],
+  ['git@github.com:feross/webcam-spy.git', 'webcam-spy']
 ]
 
 series(MODULES.map(mod => {
